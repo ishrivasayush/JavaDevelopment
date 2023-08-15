6c60927c1065d657c5a8a944e04b4c3d7ff0b0c8
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-08-15 20:37:11 +0530

    day2

---
+++ src/DataType/DataSizeRange.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class DataSizeRange{
+public static void main(String args[])
+{
+int x;
+Integer y;
+System.out.println(Integer.MIN_VALUE);
+System.out.println(Integer.MAX_VALUE);
+System.out.println(Integer.BYTES);
+}
+
+}
